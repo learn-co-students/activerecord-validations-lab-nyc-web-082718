afa7b6e0d9dcd61c2efd1e25aa7e05c8ed10a3fb
yapjinai <jinai.jyap@gmail.com>
2018-09-23 11:38:45 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+require 'pry'
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validate :clickbait
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+  def clickbait
+    if title && !title.match(/(Won't Believe)|(Secret)|(Top [0-9]+)|(Guess)/)
+      errors.add(:title, "is not clickbaity enough")
+    end
+  end
 end
