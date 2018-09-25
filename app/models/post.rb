6525b69ef6562a613ba5c81f37704223e12b9c43
github username <github email address>
2018-09-25 17:57:40 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
+require 'pry'
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 11}
+  validates :summary, length: {maximum: 10}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :click_bait?
+
+  def click_bait?
+    status = false
+    check_word = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    if self.title
+    check_word.each do |w|
+       if self.title.include?(w) == true
+         status = true
+         break
+       end
+    end
+  end
+    status
+  end
+
 end
