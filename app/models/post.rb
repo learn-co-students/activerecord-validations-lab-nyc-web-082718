b4bbc0296fc59b6d940d84b21c9ae3a6924a998b
JStern95 <JSternberg95@gmail.com>
2018-09-25 13:04:17 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ActiveRecord::Base
-end
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :clickbait?
+
+#   def clickbait?
+#     if self.title == nil
+#         false
+#     else
+#       unless self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Top") || self.title.include?("Guess")
+#         errors.add(:title, "Do you WANT Troy to get his axe?")
+#       end
+#     end
+#     end
+#
+# end
+
+
+  def clickbait?
+    array = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+    if title && array.none? { |word| title.include?(word) }
+      errors.add(:title, "Do you WANT Troy to get his axe?")
+      end
+    end
+  end
